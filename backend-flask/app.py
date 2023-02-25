b0dbc273d1593c102205525fe8704a70ff886b4f
atomicperli <yashwanth.perli@gmail.com>
2023-02-25 18:55:07 +0000

    telemetry with tracer

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -20,6 +20,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry import trace
 
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
